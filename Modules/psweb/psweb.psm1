efc2c75faecccddf0a7fbc28e01dffa1a97b2a18
lukemgriffith <lukemgriffith@gmail.com>
2017-01-14 12:41:31 +0000

    testing fix

--- Modules/psweb/psweb.psm1
+++ Modules/psweb/psweb.psm1
@@ -1,5 +1,13 @@
 using namespace System.Collections.Generic
 using namespace System.Net
+using Module .\Controller.psm1
+
+
+[RegisterController]::InitializeRegister()
+[RegisterController]::Register(
+    [Controller]::new()
+)
+
 
 # Loading functions
 Get-ChildItem $PSScriptRoot -Recurse -Filter "*.ps1" | ForEach-Object -Process {
